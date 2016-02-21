a75815eebcafbcddbed4da2055a1b8657b0d5b00
Andrew Gallagher <kivlor@gmail.com>
2016-02-21 17:37:20 +1000

    linted and formatted

--- itcrowdipsum_test.go
+++ itcrowdipsum_test.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestGenerateLispum(t *testing.T) {
-    var subject []string
+	var subject []string
 
-    subject = GenerateLipsum(5)
+	subject = GenerateLipsum(5)
 
-    if len(subject) != 5 {
-        t.Error("expected a len of 5, got ", len(subject))
-    }
+	if len(subject) != 5 {
+		t.Error("expected a len of 5, got ", len(subject))
+	}
 
-    subject = GenerateLipsum(1)
+	subject = GenerateLipsum(1)
 
-    if len(subject) != 1 {
-        t.Error("expected a len of 1, got ", len(subject))
-    }
+	if len(subject) != 1 {
+		t.Error("expected a len of 1, got ", len(subject))
+	}
 }
