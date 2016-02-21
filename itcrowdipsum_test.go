9d81bb7723af1834f33fdafbde952d774d9788de
Andrew Gallagher <kivlor@gmail.com>
2016-02-21 17:21:16 +1000

    added a test for the generate lipsum func

---
+++ itcrowdipsum_test.go
@@ -0,0 +1,21 @@
+package itcrowdipsum
+
+import (
+    "testing"
+)
+
+func TestGenerateLispum(t *testing.T) {
+    var subject []string
+
+    subject = GenerateLipsum(5)
+
+    if len(subject) != 5 {
+        t.Error("expected a len of 5, got ", len(subject))
+    }
+
+    subject = GenerateLipsum(1)
+
+    if len(subject) != 1 {
+        t.Error("expected a len of 1, got ", len(subject))
+    }
+}
